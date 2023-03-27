b853cb41ae1f3bf20a998ebb990fe32ca789fd64
𝙴𝚖𝚖𝚊𝚗𝚞𝚎𝚕 𝙱𝚊𝚛𝚛𝚎𝚛𝚊❤ <emmanuelbarreragil@MacBook-Pro-de-Emmanuel.local>
2023-03-27 14:54:22 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+const mensaje = () => {
+  return <h1>Hola Mundo</h1>
 }
 
+const Description = () => {
+  return <p>Esta es la App del Curso de fullstack bootcamp</p>
+}
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Mensaje color='red' message='Estamos trabajando' />
+      <Mensaje color='green' message='En un curso' />
+      <Mensaje color='yellow' message='de React' />
+      <Description />
+    </div>
+  );
+};
 export default App;
